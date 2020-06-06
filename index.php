49372b7bc6717055d7f50c2f721d6965f33a88ea
gusramireles46 <gusramireles46@gmail.com>
2020-06-06 00:19:52 -0500

    probando material design

--- index.php
+++ index.php
@@ -3,9 +3,24 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/material.css">
+        <script src="js/material.js"></script>
         <title>Inicio | TiendaPHP</title>
     </head>
     <body>
-        
+        <header>
+            <div class="nav">
+                <h2>Tienda de articulos</h2>
+                <div class="menu">
+                    <ul>
+                        <li>Elemento 1</li>
+                        <li>Elemento 2</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="contenedor">
+        <input class="mdl-button mdl-button--colored" type="submit" value="Hola"></input>
+        </div>
     </body>
 </html>
